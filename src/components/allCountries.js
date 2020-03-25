95dc131f1be588adbe57abdcfb93d1ce0280ea9c
dariusmaximus <dariulqa@wp.pl>
2020-03-26 00:13:02 +0100

    all countries component styles added

--- src/components/allCountries.js
+++ src/components/allCountries.js
@@ -4,9 +4,12 @@ import Country from "./country";
 function AllCountries({ countries }) {
   const [sortedCountries, setSortedCountries] = useState([]);
 
-  // useEffect(() => {
-  //     console.log(sortedCountries)
-  // }, [sortedCountries])
+  useEffect(() => {
+    let sortedResults = [...countries].sort((a, b) => {
+        return b.TotalConfirmed - a.TotalConfirmed;
+      }) 
+    setSortedCountries(sortedResults);
+  }, [])
 
   const sortResults = (val) => {
     let sortedResults = [...countries].sort((a, b) => {
@@ -24,9 +27,11 @@ function AllCountries({ countries }) {
   return (
     <div className="all-container">
       <p className="all-text">Sort countries by the highest number of:</p>
-      <button className="all-btn" onClick={() => sortResults("deaths")}>Deaths</button>
-      <button className="all-btn" onClick={() => sortResults("cases")}>Cases</button>
-      <button className="all-btn" onClick={() => sortResults("recovered")}>Recoveries</button>
+      <div className="all-btns">
+      <button className="all-btns__btn" onClick={() => sortResults("cases")}>Cases</button>
+      <button className="all-btns__btn" onClick={() => sortResults("deaths")}>Deaths</button>
+      <button className="all-btns__btn" onClick={() => sortResults("recovered")}>Recoveries</button>
+      </div>
       <ul className="all-countries">
         {sortedCountries.map((country, key) => {
           return <Country key={key} country={country}/>
--- src/components/country.js
+++ src/components/country.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactCountUp from "react-countup-v2";
 
 function Country({ country }) {
   let imgSrc = `https://cdn.countryflags.com/thumbs/${country.Slug === 'us' ? 'united-states-of-america' : country.Slug}/flag-400.png`;
@@ -9,9 +10,9 @@ function Country({ country }) {
         {/* <img src={imgSrc}></img> */}
         <p className="all-countries__item-country">{country.Country}</p>
       </div>
-      <p className="all-countries__item-cases">Confirmed cases: <span>{country.TotalConfirmed}</span></p>
-      <p className="all-countries__item-deaths">Total deaths: <span>{country.TotalDeaths}</span></p>
-      <p className="all-countries__item-recovered">Recovered: <span>{country.TotalRecovered}</span></p>
+      <p className="all-countries__item-data">Confirmed: <span><ReactCountUp endVal={country.TotalConfirmed} /></span></p>
+      <p className="all-countries__item-data">Total deaths: <span><ReactCountUp endVal={country.TotalDeaths} /></span></p>
+      <p className="all-countries__item-data">Recovered: <span><ReactCountUp endVal={country.TotalRecovered}/></span></p>
     </li>
   );
 }
